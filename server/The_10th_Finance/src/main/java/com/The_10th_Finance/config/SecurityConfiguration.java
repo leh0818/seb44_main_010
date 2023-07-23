e3c1c0db600c31466ffa5597f19131a3aa5194ae
leh0818 <65188639+leh0818@users.noreply.github.com>
2023-07-23 22:42:39 +0900

    Update SecurityConfiguration.java

--- server/The_10th_Finance/src/main/java/com/The_10th_Finance/config/SecurityConfiguration.java
+++ server/The_10th_Finance/src/main/java/com/The_10th_Finance/config/SecurityConfiguration.java
@@ -73,7 +73,6 @@ public class SecurityConfiguration {
 //                .oauth2Login(oauth2 -> oauth2
 //                .successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, userService)))
                 .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers("/payment").permitAll()
                         .antMatchers("/payment/complete").hasRole("USER")
                         .antMatchers("/payment/sum").hasRole("USER")
                         .antMatchers("/main/{userId}/{Month}").hasRole("USER")
